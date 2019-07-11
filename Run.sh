62db11193289f5936a3e62b85d8f83e40709e3a0
BenR <ben.riley@sweagle.com>
2019-07-11 12:05:11 +0100

    Commit

--- Run.sh
+++ Run.sh
@@ -1,9 +1,12 @@
 #############
-//Uploads config into Machine Learning Repo.
+###//Uploads config into Machine Learning Repo.
+###
 
-curl -X POST \
-https://devtest2.sweagle.com/api/v1/tenant/metadata-parser/parse -H "Authorization: bearer d910c7f4-182d-4830-881a-a13ad3abc451" -d "mds=GoogleCloud&parser=all&args=&format=YAML" -O /Users/boondock/Documents/GitHub/MachineLearning/GoogleCloud.yaml
+echo 
+curl -X POST https://devtest2.sweagle.com/api/v1/tenant/metadata-parser/parse -H "Authorization: bearer d910c7f4-182d-4830-881a-a13ad3abc451" -d "mds=GoogleCloud&parser=all&args=&format=YAML" -o /Users/boondock/Documents/GitHub/MachineLearning/GoogleCloud.yaml
 
-Git add .
-Git commit -m "Alter File"
-Git push origin master
+echo Git add .
+echo Git commit -m "Alter File"
+echo Git push origin master
+
+
